42ae667436f6d9f8c533f1fbcac15279685e8e3a
Apoorva Anand <apoorvaanand.work@gmail.com>
2019-07-13 23:53:49 +0530

    Change ini file site name

--- stream_sub.py
+++ stream_sub.py
@@ -3,9 +3,12 @@ from db import *
 
 create_table()
 
-reddit = praw.Reddit('RepostBot')
+reddit = praw.Reddit('AA')
 
 for submission in reddit.subreddit('AABotTesting').stream.submissions():
     insert_sub(submission.id, submission.is_self, submission.url, submission.permalink)
+    
+    if submission.is_self:
+        upd_proc(submission.id)
 
     
