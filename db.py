74b0b9ef67d3bc3ed9059e7d6ccd675572e1b031
Apoorva Anand <apoorvaanand.work@gmail.com>
2019-07-13 14:59:48 +0530

    Add streamed subs to db for further processing

---
+++ db.py
@@ -0,0 +1,44 @@
+import psycopg2
+from db_con import config
+ 
+def create_table():
+    command = """CREATE TABLE IF NOT EXISTS subdata (
+                   id VARCHAR(10) PRIMARY KEY,
+                   is_self BOOL,
+                   url TEXT,
+                   permalink TEXT,
+                   processed BOOL
+               )"""                  
+    conn = None
+    try:
+        params = config()
+        conn = psycopg2.connect(**params)
+        cur = conn.cursor()
+
+        cur.execute(command)
+        cur.close()
+        conn.commit()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+
+def insert_sub(id, is_self, url, permalink, processed=False):
+    command = """INSERT INTO subdata VALUES (%s, %s, %s, %s, %s)
+                 ON CONFLICT 
+                 DO NOTHING"""
+    conn = None
+    try:
+        params = config()
+        conn = psycopg2.connect(**params)
+        cur = conn.cursor()
+
+        cur.execute(command, [id, is_self, url, permalink, processed])
+        cur.close()
+        conn.commit()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
--- stream_sub.py
+++ stream_sub.py
@@ -1,6 +1,11 @@
 import praw
+from db import *
+
+create_table()
 
 reddit = praw.Reddit('RepostBot')
 
 for submission in reddit.subreddit('AABotTesting').stream.submissions():
-    print(submission)
+    insert_sub(submission.id, submission.is_self, submission.url, submission.permalink)
+
+    
